fcf993ce0739acc638a3ef8a073d08831a019139
Tommy <44683197+Tommysvarva@users.noreply.github.com>
2019-11-28 12:55:21 +0100

    s

---
+++ caretag/Login.js
@@ -0,0 +1,13 @@
+login();
+function login() {
+    main.innerHTML = `
+                    <div id="NorwegianFlag" style="width: 50px; height: 50px;"><img src="https://discordapp.com/assets/1d2976b1e227bd271bcbcf3e3da9e6fc.svg"/></div>
+                    <div id="UKFlag"></div>
+                    <div></div>
+
+
+
+                    <div id="passwordBox">
+                    Password
+                    </div>`
+}
---
+++ caretag/glemtpassord.js
@@ -0,0 +1,11 @@
+login();
+function login() {
+    main.innerHTML = `
+    <h1>Glemt Passord</h1>
+    
+    <div> Email/Tlf </div>
+    <input>
+    <div></div>
+
+    <button>Motta mail/sms</button>
+}
